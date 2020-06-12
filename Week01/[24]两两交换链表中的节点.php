5f3d636097d4f68c586a737125d5e38ff197f82f
zealyw <zealyw@qq.com>
2020-06-13 00:19:06 +0800

    两两交换链表节点

---
+++ Week01/[24]两两交换链表中的节点.php
@@ -0,0 +1,48 @@
+<?php
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 1->2->3->4, 你应该返回 2->1->4->3.
+// 
+// Related Topics 链表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function swapPairs($head) {
+        if($head == null || $head->next == null){
+            return $head;
+        }
+        $listNode = new ListNode(null);
+        $listNode->next = $head;
+        $cur = $listNode;
+        while ($cur->next != null && $cur->next->next != null){
+            $first = $cur->next;
+            $second = $cur->next->next;
+            $cur->next = $second;
+            $first->next = $second->next;
+            $second->next = $first;
+            $cur = $cur->next->next;
+        }
+        return $listNode->next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
